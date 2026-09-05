a6c79e2885c282d58902c10d42160296692ba82a
Kalle Fagerberg <dev@applejag.eu>
2026-09-05 10:44:49 +0200

    Simplify example

--- firefly/net_example_test.go
+++ firefly/net_example_test.go
@@ -2,21 +2,10 @@ package firefly_test
 
 import "github.com/firefly-zero/firefly-go/firefly"
 
-var listOfPlayers []Player
-
-type Player struct {
-	Pos   firefly.Point
-	Angle firefly.Angle
-	Peer  firefly.Peer
-}
-
 func ExampleScanPeers() {
 	scanner := firefly.ScanPeers()
 	for scanner.Scan() {
-		listOfPlayers = append(listOfPlayers, Player{
-			Pos:   firefly.P(0, 0),
-			Angle: firefly.Radians(0),
-			Peer:  scanner.Peer(),
-		})
+		peer := scanner.Peer()
+		_ = peer
 	}
 }
